91b1acdc5c82b950e73729715293b1a1824bd5bf
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-04-30 16:22:34 +0200

    lista cognomi

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-/* 
-1. Chiedi all'utente il cognome
-2. Inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
-3. Stampa la lista ordinata alfabeticamente
-4. Scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
- */
+ 
+// 1. Chiedi all'utente il cognome
+// 2. Inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
+// 3. Stampa la lista ordinata alfabeticamente
+// 4. Scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+ 
+// 1. Chiedi all'utente il cognome
+var listaCognomi = ["Bianche", "Neri", "Rossi", "Verdi", "Gialli"]
+console.log("Lista iniziale:", listaCognomi);
+document.getElementById("surname").innerHTML = listaCognomi;
 
+
+// 2. Inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
+
+// 3. Stampa la lista ordinata alfabeticamente
+
+// 4. Scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
