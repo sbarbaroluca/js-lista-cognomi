b35e7788e3bd3a614ad51d2f7c746b9c446d5642
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-04-30 16:54:53 +0200

    lista + aggiunta utente

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@
 // 3. Stampa la lista ordinata alfabeticamente
 // 4. Scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
  
-// 1. Chiedi all'utente il cognome
+// 1 Creare una la lista dei cognomi
 var listaCognomi = ["Bianche", "Neri", "Rossi", "Verdi", "Gialli"]
 console.log("Lista iniziale:", listaCognomi);
 document.getElementById("surname").innerHTML = listaCognomi;
 
+// 2 Chiedere all'utente il cognome ed inserirlo nella lista
+listaCognomi.push(prompt("Scrivi il tuo cognome"));
+console.log("Lista dopo l'aggiunta:", listaCognomi);
+document.getElementById("surname-add").innerHTML = listaCognomi;
 
-// 2. Inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
-
-// 3. Stampa la lista ordinata alfabeticamente
-
-// 4. Scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+//3 Stampa della lista dei cognomi in ordine alfabetico
