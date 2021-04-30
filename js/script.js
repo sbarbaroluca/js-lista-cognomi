3823b21d527ff1a01caa480055e7fb32956c2a87
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-04-30 16:00:12 +0200

    inizio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* 
+1. Chiedi all'utente il cognome
+2. Inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
+3. Stampa la lista ordinata alfabeticamente
+4. Scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+ */
+
